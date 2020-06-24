613d386aacf3ee450e2f884536b06afda5832965
edures017 <edures017@gmail.com>
2020-06-24 20:19:32 +0530

    Updates

---
+++ precache-manifest.f6161ab90a7e3289cfd29721808cb2f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ee1d9edeb441dfae9861241d94060b",
+    "url": "/EduRes-web/index.html"
+  },
+  {
+    "revision": "abc707ab0608cf582dc7",
+    "url": "/EduRes-web/static/css/main.016f11ac.chunk.css"
+  },
+  {
+    "revision": "5a4b679361549ef5f268",
+    "url": "/EduRes-web/static/js/2.964526b4.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/EduRes-web/static/js/2.964526b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc707ab0608cf582dc7",
+    "url": "/EduRes-web/static/js/main.c3515517.chunk.js"
+  },
+  {
+    "revision": "0ed10c6da292aa075244",
+    "url": "/EduRes-web/static/js/runtime-main.843c2e6b.js"
+  }
+]);
